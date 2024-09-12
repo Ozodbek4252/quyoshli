38237d900e5c0ea2bc05507ad98f3fe5af661c4b
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-09-12 10:14:43 +0500

    admin lang

---
+++ lang/uz/admin.php
@@ -0,0 +1,314 @@
+<?php
+
+return [
+    'home' => 'Asosiy',
+    'edit' => 'Tahrirlash',
+    'delete' => 'O`chirish',
+    'actions' => 'Harakatlar',
+    'cancel' => 'Bekor qilish',
+    'are_you_sure' => 'Ishonchingiz komilmi',
+    'are_you_sure_block' => 'Ishonchingiz komilmi',
+    'are_you_sure_block_raz' => 'Ishonchingiz komilmi',
+    'add' => 'Qo`shish',
+    'more' => 'Batafsil',
+    'save' => 'Saqlash',
+    'see' => 'Ko`rish',
+    'ye' => 'so`m',
+    'info' => 'Ma`lumot',
+    'no_data' => 'Ma`lumot yo`q',
+    'no_publish' => 'Nashr qilinmagan',
+    'back' => 'Orqaga',
+    'all_fields_with' => 'Barcha * bilan belgilangan maydonlar majburiy',
+    'preview' => 'Ko`rish',
+
+    'permissions' => [
+        'view' => 'Ko`rish',
+        'print' => 'Chop etish',
+        'create' => 'Qo`shish',
+        'update' => 'Tahrirlash',
+        'delete' => 'O`chirish',
+        'delivery' => 'Yetkazib berishni tahrirlash',
+        'status' => 'Holatni tahrirlash :status',
+        'filter' => 'Filtrga kirishni ruxsat etish'
+    ],
+
+    'settings' => [
+        'title' => 'Sozlamalar',
+        'name' => 'Sayt nomi',
+        'phone' => 'Telefon',
+        'phone_other' => 'Boshqa telefon',
+        'email' => 'Elektron pochta',
+        'address' => 'Manzil',
+        'telegram' => 'Telegram',
+        'facebook' => 'Facebook',
+        'instagram' => 'Instagram',
+        'keywords' => 'Kalit so`zlar',
+        'description' => 'Sayt tavsifi',
+        'landmark' => 'Landmark',
+        'day_delivery' => 'Yetkazib berish vaqti',
+        'price_delivery' => 'Yetkazib berish narxi',
+    ],
+
+    'delivery' => [
+        'title' => 'Yetkazib berish'
+    ],
+
+    'categories' => [
+        'title' => 'Kategoriyalar',
+        'name' => 'Nomi',
+        'image' => 'Faqat fayllar turi: jpg, jpeg, png',
+        'position' => 'Pozitsiya',
+        'choose_brand' => 'Brend(lar)ni tanlang',
+        'add_cat' => 'Kategoriyaga biriktirish',
+        'remove_cat' => 'Kategoriyadan olib tashlash',
+        'sub_category' => 'Asosiy kategoriya',
+        'choose_cat' => 'Kategoriyani tanlang',
+
+        'char' => [
+            'name' => 'Maydon nomi',
+            'type' => 'Maydon turi'
+        ]
+    ],
+
+    'colors' => [
+        'title' => 'Ranglar',
+        'name' => 'Nomi',
+        'color' => 'Rang',
+    ],
+
+    'users' => [
+        'title' => 'Foydalanuvchilar',
+        'fio' => 'To`liq ism',
+        'username' => 'Login',
+        'phone' => 'Telefon',
+        'ip' => 'IP',
+        'first_name' => 'Ism',
+        'last_name' => 'Familiya',
+        'role' => 'Rol',
+        'date_reg' => 'Ro`yxatdan o`tgan sana',
+        'date_online' => 'So`nggi kirgan sana',
+    ],
+
+    'staffs' => [
+        'title' => 'Xodimlar',
+        'password' => 'Parol',
+    ],
+
+    'products' => [
+        'title' => 'Mahsulotlar',
+        'name' => 'Nomi',
+        'body' => 'Tavsif',
+        'image' => 'Rasm',
+        'category' => 'Kategoriya',
+        'price' => 'Narx',
+        'price_discount' => 'Chegirma narxi',
+        'screens' => 'Ekranlar',
+        'publish' => 'Nashr qilish',
+        'search' => 'ID yoki Nomi',
+        'brands' => 'Brendlar',
+        'brand' => 'Brend',
+        'characteristics' => 'Xususiyatlar',
+        'delete_mass' => 'Massov o`chirish',
+        'status-active' => 'Aktivlashtirish',
+        'deactivate' => 'Deaktivlashtirish',
+    ],
+
+    'compilations' => [
+        'title' => 'Tayyorlovlar',
+    ],
+
+    'orders' => [
+        'title' => 'Buyurtmalar',
+        'user' => 'Foydalanuvchi',
+        'city' => 'Shahar',
+        'address' => 'Manzil',
+        'status' => 'Holat',
+        'delivery_type' => 'Yetkazib berish turi',
+        'payment_system' => 'To`lov turi',
+        'transaction' => 'Tranzaksiya',
+        'date' => 'Sana',
+        'order' => 'Buyurtma',
+        'order_number' => 'Buyurtma raqami №:number',
+        'client' => 'Mijoz',
+        'floor' => 'Qavat',
+        'entrance' => 'Kirish',
+        'house' => 'Uy',
+        'first_name' => 'Ism',
+        'last_name' => 'Familiya',
+        'phone' => 'Telefon',
+        'product' => 'Mahsulotlar',
+        'count' => 'Soni',
+        'size' => 'O`lcham',
+        'price' => 'Narx',
+        'total' => 'Jami',
+        'color' => 'Rang',
+        'article_number' => 'Artikul',
+        'delivery' => 'Yetkazib berish',
+        'branch' => 'Filial',
+        'shipment_date' => 'Olib ketish sanasi',
+        'comment' => 'Izoh'
+    ],
+
+    'billing' => [
+        'title' => 'Hisob-kitob',
+        'user' => 'Foydalanuvchi',
+        'amount' => 'Summa',
+        'order_id' => 'Buyurtma',
+        'status' => 'Holat',
+        'payed' => 'To`landi',
+        'waiting' => 'Kutilmoqda',
+        'refused' => 'Rad etildi',
+        'date' => 'Sana',
+    ],
+
+    'logs' => [
+        'title' => 'Loglar',
+        'user' => 'Foydalanuvchi',
+        'action' => 'Harakatlar',
+        'section' => 'Bo`lim'
+    ],
+
+    'posts' => [
+        'title' => 'Postlar',
+        'image' => 'Rasm',
+        'name' => 'Nomi',
+        'views' => 'Ko`rishlar',
+        'language' => 'Til',
+        'top' => 'Tepada',
+        'content' => 'Matn',
+        'type' => [
+            'title' => 'Post turi',
+            'news' => 'Yangiliklar',
+            'article' => 'Maqolalar',
+            'sales' => 'Aksiyalar',
+            'media' => 'Media biz haqida',
+        ]
+    ],
+
+    'brands' => [
+        'title' => 'Brendlar',
+        'image' => 'Rasm',
+        'name' => 'Nomi',
+    ],
+
+    'partners' => [
+        'title' => 'Hamkorlar',
+        'image' => 'Rasm',
+        'name' => 'Nomi',
+    ],
+
+    'files' => [
+        'title' => 'Fayllar',
+        'size' => 'Hajmi',
+        'path' => 'Fayl manzili',
+    ],
+
+    'branches' => [
+        'title' => 'Filiallar',
+        'name' => 'Nomi',
+        'phone' => 'Telefon',
+        'address' => 'Manzil',
+        'schedule' => 'Filial ish vaqtlari',
+        'map' => 'Xarita havolasi',
+        'orientation' => 'Orientatsiya'
+    ],
+
+    'slider' => [
+        'title' => 'Bannerni',
+        'name' => 'Nomi',
+        'discount' => 'Chegirma',
+        'link' => 'Havola',
+        'image' => 'Rasm',
+        'position' => 'Pozitsiya',
+        'type' => 'Turi',
+        'lang' => 'Til',
+        'placement' => [
+            'title' => 'Joylashuv',
+            'choose' => 'Joylashuvni tanlang',
+            'top' => 'Yuqori banner',
+            'middle' => 'Pastki banner'
+        ]
+    ],
+
+    'specialOffers' => [
+        'title' => 'Maxsus takliflar',
+        'name' => 'Nomi',
+        'link' => 'Havola',
+        'image' => 'Rasm',
+        'description' => 'Tavsif',
+    ],
+
+    'address' => [
+        'title' => 'Manzillar',
+    ],
+
+    'roles' => [
+        '1' => 'Administrator',
+        '2' => 'Foydalanuvchi',
+        '3' => 'Muharrir',
+        'title' => 'Rollar',
+        'name' => 'Nomi',
+    ],
+
+    'messages' => [
+        'updated' => 'Muvaffaqiyatli yangilandi',
+        'created' => 'Muvaffaqiyatli yaratildi',
+        'deleted' => 'Muvaffaqiyatli o`chirildi',
+        'uploaded' => 'Fayl muvaffaqiyatli yuklandi',
+        'published' => 'Muvaffaqiyatli nashr qilindi',
+        'success' => 'Amal muvaffaqiyatli bajarildi'
+    ],
+
+    'pages' => [
+        'title' => 'Sahifalar',
+        'name' => 'Nomi',
+        'description' => 'Tavsif',
+    ],
+
+    'feedback' => [
+        'title' => 'Fikrlar',
+        'name' => 'Ism',
+        'phone' => 'Telefon',
+        'created_at' => 'Sana',
+        'status' => 'Holat',
+        'viewed' => 'Ko`rildi',
+        'not_viewed' => 'Ko`rilmagan',
+        'message' => 'Xabar'
+    ],
+
+    'comments' => [
+        'title' => 'Izohlar',
+        'full_name' => 'To`liq ism',
+        'star' => 'Reyting',
+        'star0' => 'Yomon',
+        'star1' => 'Yomon emas',
+        'star2' => 'Oddiy',
+        'star3' => 'Yaxshi',
+        'status' => 'Holat',
+        'published' => 'Nashr qilindi',
+        'not_published' => 'Nashr qilinmagan',
+        'publish' => 'Nashr qilish'
+    ],
+
+    'regions' => [
+        'title' => 'Viloyatlar',
+        'name' => 'Nomi',
+    ],
+
+    'cities' => [
+        'title' => 'Shaharlar',
+        'name' => 'Nomi',
+        'choose_region' => 'Viloyatni tanlang'
+    ],
+
+    'currency' => [
+        'title' => 'Valyutalar',
+        'dollar' => 'Dollar',
+        'euro' => 'Yevro',
+        'created_at' => 'Sana'
+    ],
+
+    'notification_available' => [
+        'title' => 'Bildirishnoma mavjud',
+    ]
+];
