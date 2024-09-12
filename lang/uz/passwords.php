d978ded1e3d2ab3c5336b9d65334cc0296a08e89
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-09-12 10:20:16 +0500

    password lang

---
+++ lang/uz/passwords.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Parolingiz qayta tiklandi!',
+    'sent' => 'Parolni tiklash havolasini elektron pochta orqali yubordik!',
+    'token' => 'Bu parolni tiklash belgisi yaroqsiz.',
+    'user' => "Biz ushbu elektron pochta manziliga ega foydalanuvchini topolmaymiz.",
+
+];
