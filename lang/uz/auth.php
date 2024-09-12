1f584150afac1001b6d87e17ac1352e72ca7f266
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-09-12 10:17:06 +0500

    auth lang

---
+++ lang/uz/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'failed' => 'Bu ma\'lumotlar bazamiz bilan mos kelmadi.',
+    'throttle' => 'Kirish uchun juda ko\'p urinishlar. Iltimos, :seconds soniya ichida qayta urinib ko\'ring.',
+
+];
